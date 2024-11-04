8dd13d6f3f9402be88f67caa97639b461b65d361
Navinofc44 <Pahasarasanujja@gmail.com>
2024-11-04 06:23:30 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94774971556"
+const SESSION_ID = "PRABATH-MD~9dZ3TD6Z#hyD1WdHbNxjKKDST__p0-ZQzy4tFNC8O4g8ACnYObfk"
 
 
 module.exports = {
